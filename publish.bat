095bac93848070c82d52aac671d6fa1f813e97f4
snltty <1069410172@qq.com>
2022-09-11 00:56:17 +0800

    完善发布命令和github自动化

--- publish.bat
+++ publish.bat
@@ -1,7 +1,10 @@
 @echo off
 
+rem 24行发布安卓那里，改为自己的Android sdk地址, 可以在 工具->选项->Xamarin->Android设置 里查看sdk地址
+
 rd /s /q public\\publish
 rd /s /q public\\publish-zip
+mkdir public\\publish-zip
 
 dotnet publish ./client/client.service -c release -f net6.0 -o ./public/publish/any/client 
 dotnet publish ./server/server.service -c release -f net6.0 -o ./public/publish/any/server 
@@ -18,21 +21,25 @@ dotnet publish ./server/server.service -c release -f net6.0 -o ./public/publish/
 dotnet publish ./client/client.service -c release -f net6.0 -o ./public/publish/osx-arm64-single/client	-r osx-arm64		--self-contained true  -p:DebugType=none -p:DebugSymbols=false  -p:PublishSingleFile=true -p:PublishTrimmed=true -p:IncludeNativeLibrariesForSelfExtract=true 
 dotnet publish ./server/server.service -c release -f net6.0 -o ./public/publish/osx-arm64-single/server	-r osx-arm64		--self-contained true  -p:DebugType=none -p:DebugSymbols=false  -p:PublishSingleFile=true -p:PublishTrimmed=true -p:IncludeNativeLibrariesForSelfExtract=true 
 
-xcopy "client/plugins/client.service.ui/client.service.ui.api.service/public/web" "public/publish/win-x64-single/client/public/web" /s /f /h /y
-xcopy "client/plugins/client.service.ui/client.service.ui.api.service/public/web" "public/publish/win-arm64-single/client/public/web" /s /f /h /y
-xcopy "client/plugins/client.service.ui/client.service.ui.api.service/public/web" "public/publish/linux-x64-single/client/public/web" /s /f /h /y
-xcopy "client/plugins/client.service.ui/client.service.ui.api.service/public/web" "public/publish/linux-arm64-single/client/public/web" /s /f /h /y
-xcopy "client/plugins/client.service.ui/client.service.ui.api.service/public/web" "public/publish/osx-x64-single/client/public/web" /s /f /h /y
-xcopy "client/plugins/client.service.ui/client.service.ui.api.service/public/web" "public/publish/osx-arm64-single/client/public/web" /s /f /h /y
+dotnet publish ./client/client.service.app -c:Release -f:net6.0-android /p:AndroidSigningKeyPass=123321 /p:AndroidSigningStorePass=123321  /p:AndroidSdkDirectory=D:\\Android\\android-sdk
+echo F|xcopy "client\\client.service.app\\bin\\Release\\net6.0-android\\publish\\p2p_tunnel.p2p_tunnel-Signed.apk" "public\\publish-zip\\p2p-tunnel.apk"  /s /f /h /y
 
-mkdir public\\publish-zip
-cd public/publish
-winrar a -r ../publish-zip/any.zip any
-winrar a -r ../publish-zip/win-x64-single.zip win-x64-single
-winrar a -r ../publish-zip/win-arm64-single.zip win-arm64-single
-winrar a -r ../publish-zip/linux-x64-single.zip linux-x64-single
-winrar a -r ../publish-zip/linux-arm64-single.zip linux-arm64-single
-winrar a -r ../publish-zip/osx-x64-single.zip osx-x64-single
-winrar a -r ../publish-zip/osx-arm64-single.zip osx-arm64-single
+echo D|xcopy "client\\plugins\\client.service.ui\\client.service.ui.api.service\\public\\web\\" "public\\publish\\win-x64-single\\client\\public\\web\\" /s /f /h /y
+echo D|xcopy "client\\plugins\\client.service.ui\\client.service.ui.api.service\\public\\web\\" "public\\publish\\win-arm64-single\\client\\public\\web\\" /s /f /h /y
+echo D|xcopy "client\\plugins\\client.service.ui\\client.service.ui.api.service\\public\\web\\" "public\\publish\\linux-x64-single\\client\\public\\web\\" /s /f /h /y
+echo D|xcopy "client\\plugins\\client.service.ui\\client.service.ui.api.service\\public\\web\\" "public\\publish\\linux-arm64-single\\client\\public\\web\\" /s /f /h /y
+echo D|xcopy "client\\plugins\\client.service.ui\\client.service.ui.api.service\\public\\web\\" "public\\publish\\osx-x64-single\\client\\public\\web\\" /s /f /h /y
+echo D|xcopy "client\\plugins\\client.service.ui\\client.service.ui.api.service\\public\\web\\" "public\\publish\\osx-arm64-single\\client\\public\\web\\" /s /f /h /y
 
-cd ../../
+echo F|xcopy "client\\plugins\\client.service.vea\\tun2socks.exe" "public\\publish\\win-x64-single\\client\\"  /f /h /y
+echo F|xcopy "client\\plugins\\client.service.vea\\wintun.dll" "public\\publish\\win-x64-single\\client\\"  /f /h /y
+echo F|xcopy "client\\plugins\\client.service.vea\\tun2socks.exe" "public\\publish\\win-arm64-single\\client\\"  /f /h /y
+echo F|xcopy "client\\plugins\\client.service.vea\\wintun.dll" "public\\publish\\win-arm64-single\\client\\"  /f /h /y
+
+7z a -tzip ./public/publish-zip/any.zip ./public/publish/any/*
+7z a -tzip ./public/publish-zip/win-x64-single.zip ./public/publish/win-x64-single/*
+7z a -tzip ./public/publish-zip/win-arm64-single.zip ./public/publish/win-arm64-single/*
+7z a -tzip ./public/publish-zip/linux-x64-single.zip ./public/publish/linux-x64-single/*
+7z a -tzip ./public/publish-zip/linux-arm64-single.zip ./public/publish/linux-arm64-single/*
+7z a -tzip ./public/publish-zip/osx-x64-single.zip ./public/publish/osx-x64-single/*
+7z a -tzip ./public/publish-zip/osx-arm64-single.zip ./public/publish/osx-arm64-single/*
