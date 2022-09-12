585fe2ab5b5f63f2dbcba0204f641bef47e318a6
snltty <1069410172@qq.com>
2022-09-12 14:03:26 +0800

    优化docker镜像大小，仅27M

--- server-docker.bat
+++ server-docker.bat
@@ -4,6 +4,13 @@ SET target=%~dp0
 SET image=%1
 if "%image%"=="" (SET image="snltty/p2p-tunnel-server")
 
-docker build -f "%target%\server\server.service\Dockerfile" --force-rm -t %image% "%target%\"
+dotnet publish ./server/server.service -c release -f net6.0 -o ./public/publish/linux-alpine-x64 -r alpine-x64 --self-contained true  -p:DebugType=none -p:DebugSymbols=false  -p:PublishSingleFile=true -p:PublishTrimmed=true -p:IncludeNativeLibrariesForSelfExtract=true
+
+echo F|xcopy "server\\server.service\\Dockerfile" "public\\publish\\linux-alpine-x64\\Dockerfile"  /s /f /h /y
+
+cd public/publish/linux-alpine-x64
+docker build -f "%target%\\public\\publish\\linux-alpine-x64\\Dockerfile" --force-rm -t %image% .
+
+cd ../../../
 
 docker push %image%
