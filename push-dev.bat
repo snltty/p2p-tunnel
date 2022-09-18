c6a8c367954d9aa29e849a2a5658fe6b64fd6c70
snltty <1069410172@qq.com>
2022-09-18 10:04:33 +0800

    优化虚拟网卡安装表现

--- push-dev.bat
+++ push-dev.bat
@@ -2,11 +2,6 @@
 
 SET comment=%1
 
-git tag -d v1.0.0.0
-git push origin --delete v1.0.0.0
-
 git add .
 git commit -m "%comment%"
-git tag -a v1.0.0.0 -m "v1.0"
 git push origin dev
-git push origin --tags
--- push-master.bat
+++ push-master.bat
@@ -1,8 +1,14 @@
 @echo off 
 
+git tag -d v1.0.0.0
+git push origin --delete v1.0.0.0
+
 git checkout master
 git reset --hard origin/dev
 git pull
 git add .
+
+git tag -a v1.0.0.0 -m "v1.0"
 git push origin master
+git push origin --tags
 git checkout dev
