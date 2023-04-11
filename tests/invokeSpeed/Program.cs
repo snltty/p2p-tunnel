65cafcaba92d6b7e6487de66f25ca19125dfabeb
snltty <1069410172@qq.com>
2023-04-12 00:51:07 +0800

    更新一些代码性能基准的对比测试

---
+++ tests/invokeSpeed/Program.cs
@@ -0,0 +1,97 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using System.Reflection;
+
+namespace invokeSpeed
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var summary = BenchmarkRunner.Run<InvokeTest>();
+            Console.ReadLine();
+        }
+    }
+
+
+    public interface IMyInterface
+    {
+        public int Method();
+    }
+    public class MyClass : IMyInterface
+    {
+        public int Method()
+        {
+            return 0;
+        }
+    }
+
+    public delegate int MyDelegate();
+
+
+    [MemoryDiagnoser]
+    public class InvokeTest
+    {
+
+        IMyInterface myInterface = new MyClass();
+        MyClass myClass = new MyClass();
+        MyDelegate myDelegate;
+        MethodInfo method;
+
+        [GlobalSetup]
+        public void Setup()
+        {
+            method = myClass.GetType().GetMethod("Method")!;
+            myDelegate = (MyDelegate)Delegate.CreateDelegate(typeof(MyDelegate), myClass, method);
+        }
+
+
+        /// <summary>
+        /// 直接调用
+        /// </summary>
+        [Benchmark]
+        public void ByClass()
+        {
+            for (int i = 0; i < 1000; i++)
+            {
+                myClass.Method();
+            }
+        }
+
+        /// <summary>
+        /// 委托调用
+        /// </summary>
+        [Benchmark]
+        public void ByDelegate()
+        {
+            for (int i = 0; i < 1000; i++)
+            {
+                myDelegate();
+            }
+        }
+
+        /// <summary>
+        /// 接口调用
+        /// </summary>
+        [Benchmark]
+        public void ByInterface()
+        {
+            for (int i = 0; i < 1000; i++)
+            {
+                myInterface.Method();
+            }
+        }
+
+        /// <summary>
+        /// 反射调用
+        /// </summary>
+        [Benchmark]
+        public void ByReflection()
+        {
+            for (int i = 0; i < 1000; i++)
+            {
+                method.Invoke(myClass,Array.Empty<object>());
+            }
+        }
+    }
+}
--- tests/packUnpackSpeed/Program.cs
+++ tests/packUnpackSpeed/Program.cs
@@ -1,10 +1,10 @@
 ﻿using BenchmarkDotNet.Attributes;
 using BenchmarkDotNet.Running;
-using common.libs;
 using common.libs.extends;
-using common.server.model;
-using common.socks5;
-using System.Net;
+using common.server;
+using common.tcpforward;
+using MessagePack;
+using System.Buffers;
 
 namespace packUnpackSpeed
 {
@@ -12,135 +12,222 @@ namespace packUnpackSpeed
     {
         static void Main(string[] args)
         {
-            var summary = BenchmarkRunner.Run<Test>();
-            //var summary = BenchmarkRunner.Run<Socks5Pack>();
+            var summary = BenchmarkRunner.Run<TcpForwardTest>();
             Console.ReadLine();
         }
     }
 
     [MemoryDiagnoser]
-    public class Test
+    public class TcpForwardTest
     {
 
-
-        [GlobalSetup]
-        public void Setup()
+        TcpForwardInfo tcpForwardInfo = new TcpForwardInfo
         {
-
-        }
-
-
-        ReceiveDataBuffer buffer = new ReceiveDataBuffer();
-        Socks5Info socks5 = new Socks5Info
-        {
-            Id = 1,
-            Data = new byte[1024],
-            Socks5Step = Socks5EnumStep.Forward,
-            TargetEP = new IPEndPoint(IPAddress.Any, 0),
-            SourceEP = new IPEndPoint(IPAddress.Any, 0)
+            AliveType = TcpForwardAliveTypes.Web,
+            Buffer = new byte[1024],
+            DataType = TcpForwardDataTypes.Forward,
+            ForwardType = TcpForwardTypes.Forward,
+            RequestId = 1,
+            SourcePort = 5000,
+            StateType = TcpForwardStateTypes.Success
         };
-        Socks5Info socks5Debytes = new Socks5Info();
-        MessageRequestWrap messageWrapDebytes = new MessageRequestWrap();
-        byte[] data = new byte[32 * 1024];
+        Memory<byte> data1kb = new byte[1*1024];
+        Memory<byte> data2kb = new byte[2*1024];
+        Memory<byte> data8kb = new byte[8*1024];
+        Memory<byte> data16kb = new byte[16*1024];
+        Memory<byte> data32kb = new byte[32*1024];
 
 
-        [Benchmark]
-        public void Test_32KB()
-        {
-            Run(data, 32 * 1024);
-        }
-
-        [Benchmark]
-        public void Test_1KB()
-        {
-            Run(data, 1024);
-        }
-        [Benchmark]
-        public void Test_2KB()
-        {
-            Run(data, 2 * 1024);
-        }
-        [Benchmark]
-        public void Test_4KB()
-        {
-            Run(data, 4 * 1024);
-        }
-        [Benchmark]
-        public void Test_8KB()
-        {
-            Run(data, 8 * 1024);
-        }
-        [Benchmark]
-        public void Test_16KB()
-        {
-            Run(data, 16 * 1024);
-        }
-
-        private void Run(byte[] data, int len)
-        {
-            int times = (1 * 1024 * 1024 * 1024) / len;
-            for (int i = 0; i < times; i++)
-            {
-                socks5.Data = data.AsMemory(0, len);
-                //序列化
-                var bytes = socks5.ToBytes(out int length);
-                //打包
-                MessageRequestWrap request = new MessageRequestWrap
-                {
-                    MessengerId = 1,
-                    RequestId = 1,
-                    Payload = bytes.AsMemory(0, length)
-                };
-                var requestBytes = request.ToArray(out length);
-
-                //粘包
-                buffer.AddRange(requestBytes, 0, length);
-                do
-                {
-                    int packageLen = buffer.Data.Span.ToInt32();
-                    if (packageLen > buffer.Size - 4)
-                    {
-                        break;
-                    }
-
-                    //去掉4个字节的长度头
-                    var readReceive = buffer.Data.Slice(0, packageLen + 4).Slice(4);
-                    //解包
-                    messageWrapDebytes.FromArray(readReceive);
-                    //反序列化
-                    socks5Debytes.DeBytes(messageWrapDebytes.Payload);
-
-
-                    buffer.RemoveRange(0, packageLen + 4);
-                } while (buffer.Size > 4);
-
-                socks5.Return(bytes);
-                request.Return(requestBytes);
-            }
-        }
-
-    }
-
-
-    [MemoryDiagnoser]
-    public class Socks5Pack
-    {
         [GlobalSetup]
         public void Setup()
         {
 
         }
 
-
-        Socks5Info info = new Socks5Info { Id = 1, Data = new byte[100], SourceEP = new IPEndPoint(IPAddress.Loopback, 5000), TargetEP = new IPEndPoint(IPAddress.Loopback, 5001) };
         [Benchmark]
-        public void Socks5PackUnPack()
+        public void MessagePack_1KB()
         {
-            var bytes = info.ToBytes(out int length);
-            Memory<byte> memory = bytes.AsMemory(0, length);
-            Socks5Info.Debytes(memory);
-            info.Return(bytes);
+            tcpForwardInfo.Buffer = data1kb;
+            byte[] bytes = MessagePackSerializer.Serialize(tcpForwardInfo);
+            TcpForwardInfo model = MessagePackSerializer.Deserialize<TcpForwardInfo>(bytes);
+        }
+        [Benchmark]
+        public void Toarray_1KB()
+        {
+            tcpForwardInfo.Buffer = data1kb;
+            byte[] bytes = tcpForwardInfo.ToBytes(out int length);
+            TcpForwardInfo model = new TcpForwardInfo();
+            model.DeBytes(bytes.AsMemory(0,length));
+            model.Return(bytes);
+        }
+
+
+        [Benchmark]
+        public void MessagePack_2KB()
+        {
+            tcpForwardInfo.Buffer = data2kb;
+            byte[] bytes = MessagePackSerializer.Serialize(tcpForwardInfo);
+            TcpForwardInfo model = MessagePackSerializer.Deserialize<TcpForwardInfo>(bytes);
+        }
+        [Benchmark]
+        public void Toarray_2KB()
+        {
+            tcpForwardInfo.Buffer = data2kb;
+            byte[] bytes = tcpForwardInfo.ToBytes(out int length);
+            TcpForwardInfo model = new TcpForwardInfo();
+            model.DeBytes(bytes.AsMemory(0, length));
+            model.Return(bytes);
+        }
+
+
+        [Benchmark]
+        public void MessagePack_8KB()
+        {
+            tcpForwardInfo.Buffer = data8kb;
+            byte[] bytes = MessagePackSerializer.Serialize(tcpForwardInfo);
+            TcpForwardInfo model = MessagePackSerializer.Deserialize<TcpForwardInfo>(bytes);
+        }
+        [Benchmark]
+        public void Toarray_8KB()
+        {
+            tcpForwardInfo.Buffer = data8kb;
+            byte[] bytes = tcpForwardInfo.ToBytes(out int length);
+            TcpForwardInfo model = new TcpForwardInfo();
+            model.DeBytes(bytes.AsMemory(0, length));
+            model.Return(bytes);
+        }
+
+
+        [Benchmark]
+        public void MessagePack_16KB()
+        {
+            tcpForwardInfo.Buffer = data16kb;
+            byte[] bytes = MessagePackSerializer.Serialize(tcpForwardInfo);
+            TcpForwardInfo model = MessagePackSerializer.Deserialize<TcpForwardInfo>(bytes);
+        }
+        [Benchmark]
+        public void Toarray_16KB()
+        {
+            tcpForwardInfo.Buffer = data16kb;
+            byte[] bytes = tcpForwardInfo.ToBytes(out int length);
+            TcpForwardInfo model = new TcpForwardInfo();
+            model.DeBytes(bytes.AsMemory(0, length));
+            model.Return(bytes);
+        }
+
+        [Benchmark]
+        public void MessagePack_32KB()
+        {
+            tcpForwardInfo.Buffer = data32kb;
+            byte[] bytes = MessagePackSerializer.Serialize(tcpForwardInfo);
+            TcpForwardInfo model = MessagePackSerializer.Deserialize<TcpForwardInfo>(bytes);
+        }
+        [Benchmark]
+        public void Toarray_32KB()
+        {
+            tcpForwardInfo.Buffer = data32kb;
+            byte[] bytes = tcpForwardInfo.ToBytes(out int length);
+            TcpForwardInfo model = new TcpForwardInfo();
+            model.DeBytes(bytes.AsMemory(0, length));
+            model.Return(bytes);
         }
     }
 
+    [MessagePackObject]
+    public sealed class TcpForwardInfo
+    {
+        public TcpForwardInfo() { }
+
+        [Key(1)]
+        public ushort SourcePort { get; set; }
+        [Key(2)]
+        public TcpForwardAliveTypes AliveType { get; set; }
+        [Key(3)]
+        public TcpForwardTypes ForwardType { get; set; }
+        [Key(4)]
+        public TcpForwardDataTypes DataType { get; set; }
+        [Key(5)]
+        public TcpForwardStateTypes StateType { get; set; }
+
+        [Key(6)]
+        public uint RequestId { get; set; }
+
+        [Key(7)]
+        [System.Text.Json.Serialization.JsonIgnore]
+        public Memory<byte> TargetEndpoint { get; set; }
+
+        [Key(8)]
+        [System.Text.Json.Serialization.JsonIgnore]
+        public Memory<byte> Buffer { get; set; }
+
+
+        [IgnoreMemberAttribute]
+        public Memory<byte> Cache { get; set; }
+
+        [IgnoreMemberAttribute]
+        [System.Text.Json.Serialization.JsonIgnore]
+        public IConnection Connection { get; set; }
+        public byte[] ToBytes(out int length)
+        {
+            length =
+                1 +  //AliveType + ForwardType
+                1 + // DataType + StateType
+                4 +
+                1 + TargetEndpoint.Length +
+                Buffer.Length;
+
+            byte[] bytes = ArrayPool<byte>.Shared.Rent(length);
+            var memory = bytes.AsMemory();
+            int index = 0;
+
+
+            bytes[index] = (byte)(((byte)AliveType - 1) << 1 | ((byte)ForwardType - 1));
+            index += 1;
+
+            bytes[index] = (byte)((byte)DataType << 4 | (byte)StateType);
+            index += 1;
+
+            RequestId.ToBytes(memory.Slice(index));
+            index += 4;
+
+            bytes[index] = (byte)TargetEndpoint.Length;
+            index += 1;
+            TargetEndpoint.CopyTo(memory.Slice(index));
+            index += TargetEndpoint.Length;
+
+            Buffer.CopyTo(memory.Slice(index));
+            index += Buffer.Length;
+            return bytes;
+        }
+        public void DeBytes(in Memory<byte> memory)
+        {
+            var span = memory.Span;
+            int index = 0;
+
+            AliveType = (TcpForwardAliveTypes)(byte)(((span[index] >> 1) & 0b1) + 1);
+            ForwardType = (TcpForwardTypes)(byte)((span[index] & 0b1) + 1);
+            index += 1;
+
+            DataType = (TcpForwardDataTypes)(byte)((span[index] >> 4) & 0b1111);
+            StateType = (TcpForwardStateTypes)(byte)(span[index] & 0b1111);
+            index += 1;
+
+            RequestId = span.Slice(index).ToUInt32();
+            index += 4;
+
+            byte epLength = span[index];
+            index += 1;
+            TargetEndpoint = memory.Slice(index, epLength);
+            index += epLength;
+
+
+
+            Buffer = memory.Slice(index);
+        }
+
+        public void Return(byte[] data)
+        {
+            ArrayPool<byte>.Shared.Return(data);
+        }
+    }
 }
