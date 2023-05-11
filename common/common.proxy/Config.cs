f8f63f7e9ddf409e1dbc9c5ed0a28795616498eb
snltty <1069410172@qq.com>
2023-05-12 00:49:25 +0800

    写防火墙，未完成

--- common/common.proxy/Config.cs
+++ common/common.proxy/Config.cs
@@ -121,7 +121,7 @@ namespace common.proxy
             {
                 byte maskLength = c.Length > 1 ? byte.Parse(c[1]) : (byte)0;
                 uint ip = BinaryPrimitives.ReadUInt32BigEndian(IPAddress.Parse(c[0]).GetAddressBytes());
-                //每天写掩码，自动计算
+                //每填写掩码，自动计算
                 if (c.Length == 1)
                 {
                     maskLength = 32;
