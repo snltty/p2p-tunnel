ca6ca1fe0a9fbe86ecf63318ef90a8916ae53b6c
snltty <1069410172@qq.com>
2023-07-19 00:32:22 +0800

    更改一些发布文件

--- publish.bat
+++ publish.bat
@@ -13,35 +13,58 @@ rem 客户端和服务端
 for %%f in (client,server) do (
 	for %%r in (win-x64,win-arm64,linux-x64,linux-arm64,osx-x64,osx-arm64) do (
 		dotnet publish ./%%f/%%f.service -c release -f net7.0 -o ./public/publish/%%r-single/%%f  -r %%r  --self-contained true -p:TieredPGO=true  -p:DebugType=none -p:DebugSymbols=false  -p:PublishSingleFile=true -p:PublishTrimmed=true -p:EnableCompressionInSingleFile=true -p:DebuggerSupport=false -p:EnableUnsafeBinaryFormatterSerialization=false -p:EnableUnsafeUTF7Encoding=false -p:HttpActivityPropagationSupport=false -p:InvariantGlobalization=true  -p:MetadataUpdaterSupport=false  -p:UseSystemResourceKeys=true  -p:TrimMode=partial
+		
+		dotnet publish ./%%f/%%f.service -c release -f net7.0 -r %%r -o ./public/publish/any/%%f/%%r  -p:PublishSingleFile=true --self-contained false
 	)
-	dotnet publish ./%%f/%%f.service -c release -f net7.0 -o ./public/publish/any/%%f 
+	rem dotnet publish ./%%f/%%f.service -c release -f net7.0 -o ./public/publish/any/%%f -p:PublishSingleFile=true --self-contained false
 )
 rem app 改为自己的Android sdk地址, 可以在 工具->选项->Xamarin->Android设置 里查看sdk地址
 rem dotnet publish ./client/client.service.app -c:Release -f:net7.0-android /p:AndroidSigningKeyPass=123321 /p:AndroidSdkDirectory=%sdkpath%
 rem echo F|xcopy "client\\client.service.app\\bin\\Release\\net7.0-android\\publish\\p2p_tunnel.p2p_tunnel-Signed.apk" "public\\publish-zip\\p2p-tunnel.apk"  /s /f /h /y
- 
+
 
 for %%r in (x64,arm64) do (
 	for %%f in (tun2socks-linux,tun2socks-osx,tun2socks-windows.exe) do (
 		del  "public\\publish\\win-%%r-single\\client\\%%f"
 		del  "public\\publish\\osx-%%r-single\\client\\%%f"
 		del  "public\\publish\\linux-%%r-single\\client\\%%f"
-	)
-	echo F|xcopy "client\\plugins\\client.service.vea\\tun2socks-linux-%%r" "public\\publish\\linux-%%r-single\\client\\tun2socks-linux"  /f /h /y
-	echo F|xcopy "client\\plugins\\client.service.vea\\tun2socks-osx-%%r" "public\\publish\\osx-%%r-single\\client\\tun2socks-osx"  /f /h /y
-	echo F|xcopy "client\\plugins\\client.service.vea\\tun2socks-windows.exe-%%r" "public\\publish\\win-%%r-single\\client\\tun2socks-windows.exe"  /f /h /y
 
+		del  "public\\publish\\any\\client\\linux-%%r\\%%f"
+		del  "public\\publish\\any\\client\\osx-%%r\\%%f"
+		del  "public\\publish\\any\\client\\win-%%r\\%%f"
+	)
 	for %%f in (wintun.dll) do (
 		del  "public\\publish\\linux-%%r-single\\client\\%%f"
 		del  "public\\publish\\osx-%%r-single\\client\\%%f"
+		del  "public\\publish\\any\\client\\linux-%%r\\%%f"
+		del  "public\\publish\\any\\client\\osx-%%r\\%%f"
+		del  "public\\publish\\any\\client\\win-%%r\\%%f"
 	)
 	for %%p in (win,linux,osx) do (
-		echo D|xcopy "client\\plugins\\client.service.ui\\client.service.ui.api.service\\public\\web\\" "public\\publish\\%%p-%%r-single\\client\\public\\web\\" /s /f /h /y
 		del  "public\\publish\\%%p-%%r-single\\server\\*.pac"
+	)
+)
+
+
+for %%r in (x64,arm64) do (
+	echo F|xcopy "client\\plugins\\client.service.vea\\tun2socks-linux-%%r" "public\\publish\\linux-%%r-single\\client\\tun2socks-linux"  /f /h /y
+	echo F|xcopy "client\\plugins\\client.service.vea\\tun2socks-osx-%%r" "public\\publish\\osx-%%r-single\\client\\tun2socks-osx"  /f /h /y
+	echo F|xcopy "client\\plugins\\client.service.vea\\tun2socks-windows.exe-%%r" "public\\publish\\win-%%r-single\\client\\tun2socks-windows.exe"  /f /h /y
+	echo F|xcopy "client\\plugins\\client.service.vea\\wintun.dll" "public\\publish\\win-%%r-single\\client\\wintun.dll"  /f /h /y
+
+	echo F|xcopy "client\\plugins\\client.service.vea\\tun2socks-linux-%%r" "public\\publish\\any\\client\\linux-%%r\\tun2socks-linux"  /f /h /y
+	echo F|xcopy "client\\plugins\\client.service.vea\\tun2socks-osx-%%r" "public\\publish\\any\\client\\osx-%%r\\tun2socks-osx"  /f /h /y
+	echo F|xcopy "client\\plugins\\client.service.vea\\tun2socks-windows.exe-%%r" "public\\publish\\any\\client\\win-%%r\\tun2socks-windows.exe"  /f /h /y
+	echo F|xcopy "client\\plugins\\client.service.vea\\wintun.dll" "public\\publish\\any\\client\\win-%%r\\wintun.dll"  /f /h /y
+
+	for %%p in (win,linux,osx) do (
+		echo D|xcopy "client\\plugins\\client.service.ui\\client.service.ui.api.service\\public\\web\\" "public\\publish\\%%p-%%r-single\\client\\public\\web\\" /s /f /h /y
 	)
 
 	echo F|xcopy "client\\plugins\\client.service.ui\\client.service.ui.api.service\\public\\client.service.tray.exe" "public\\publish\\win-%%r-single\\client\\client.service.tray.exe"  /s /f /h /y
+	echo F|xcopy "client\\plugins\\client.service.ui\\client.service.ui.api.service\\public\\client.service.tray.exe" "public\\publish\\any\\client\\win-%%r\\client.service.tray.exe"  /s /f /h /y
 	echo F|xcopy "server\\server.service\\public\\server.service.tray.exe" "public\\publish\\win-%%r-single\\server\\server.service.tray.exe"  /s /f /h /y
+	echo F|xcopy "server\\server.service\\public\\server.service.tray.exe" "public\\publish\\any\\server\\win-%%r\\server.service.tray.exe"  /s /f /h /y
 	
 )
 del  "public\\publish\\any\\server\\*.pac"
