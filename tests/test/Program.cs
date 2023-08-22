2e9fab7b0aa0d0ee19b3d927ea120b161e6a21fc
snltty <1069410172@qq.com>
2023-08-23 00:04:56 +0800

    同步

--- tests/test/Program.cs
+++ tests/test/Program.cs
@@ -11,8 +11,7 @@ namespace test
     {
         static unsafe void Main(string[] args)
         {
-            Console.WriteLine(string.Join(",",BitConverter.GetBytes((ushort)53)));
-           //BenchmarkRunner.Run<Test>();
+            BenchmarkRunner.Run<Test>();
         }
     }
 
@@ -24,22 +23,57 @@ namespace test
         {
         }
 
-        byte[] bytes = new byte[] { 0, 0, 48, 57 };
+        byte[] bytes = new byte[24];
 
         [Benchmark]
         public unsafe void Test1()
         {
-            fixed (byte* p = &bytes[0])
-            {
-                ushort port1 = (ushort)((*(p + 2) << 8 & 0xFF00) | *(p + 3));
-            }
+            var span = bytes.AsSpan();
+            ushort addressFamily = BitConverter.ToUInt16(bytes, 0);
+            ushort port = BitConverter.ToUInt16(bytes, 2);
+            uint ipv4 = BitConverter.ToUInt16(bytes, 4);
+            ushort ipv61 = BitConverter.ToUInt16(bytes, 8);
+            ushort ipv62 = BitConverter.ToUInt16(bytes, 16);
+            TextStruct textStruct = new TextStruct(addressFamily, port, ipv4, ipv61, ipv62);
+            TestFunc2(textStruct);
         }
 
         [Benchmark]
         public void Test2()
         {
-            ushort port = BinaryPrimitives.ReverseEndianness(bytes.AsSpan(2, 2).ToUInt16());
+            byte[] res = new byte[bytes.Length];
+            bytes.AsSpan().CopyTo(res);
+            TestFunc(res);
         }
 
+        private unsafe void TestFunc(byte[] arr)
+        {
+            fixed (void* p = arr)
+            {
+                ushort port = *((byte*)p + 2);
+            }
+        }
+        private void TestFunc2(TextStruct s)
+        {
+
+            ushort port = s.Port;
+        }
+
+        public readonly record struct TextStruct
+        {
+            public TextStruct(ushort addressFamily, ushort port, uint ipv4, ulong ipv61, ulong ipv62)
+            {
+                AddressFamily = addressFamily;
+                Port = port;
+                Ipv4 = ipv4;
+                Ipv61 = ipv61;
+                Ipv62 = ipv62;
+            }
+            public readonly ushort AddressFamily;
+            public readonly ushort Port;
+            public readonly uint Ipv4;
+            public readonly ulong Ipv61;
+            public readonly ulong Ipv62;
+        }
     }
 }
