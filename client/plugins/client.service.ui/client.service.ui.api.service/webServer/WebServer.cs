fff76e6cd33637bde261a1346c921698b9eb8aaa
snltty <1069410172@qq.com>
2023-09-16 20:44:36 +0800

    更新

--- client/plugins/client.service.ui/client.service.ui.api.service/webServer/WebServer.cs
+++ client/plugins/client.service.ui/client.service.ui.api.service/webServer/WebServer.cs
@@ -25,51 +25,15 @@ namespace client.service.ui.api.service.webServer
         /// </summary>
         public void Start()
         {
-
-
             Task.Factory.StartNew(() =>
             {
                 try
                 {
                     HttpListener http = new HttpListener();
+                    http.IgnoreWriteExceptions = true;
                     http.Prefixes.Add($"http://{config.Web.BindIp}:{config.Web.Port}/");
                     http.Start();
-
-                    while (true)
-                    {
-                        HttpListenerContext context = http.GetContext();
-                        HttpListenerRequest request = context.Request;
-                        using HttpListenerResponse response = context.Response;
-                        using Stream stream = response.OutputStream;
-
-                        try
-                        {
-                            response.Headers.Set("Server", "snltty");
-
-                            string path = request.Url.AbsolutePath;
-                            //默认页面
-                            if (path == "/") path = "index.html";
-
-                            byte[] bytes = webServerFileReader.Read(path, out DateTime last);
-                            if (bytes.Length > 0)
-                            {
-                                response.ContentLength64 = bytes.Length;
-                                response.ContentType = GetContentType(path);
-                                response.Headers.Set("Last-Modified",last.ToString());
-                                stream.Write(bytes, 0, bytes.Length);
-                            }
-                            else
-                            {
-                                response.StatusCode = (int)HttpStatusCode.NotFound;
-                            }
-                        }
-                        catch (Exception)
-                        {
-                            response.StatusCode = (int)HttpStatusCode.BadRequest;
-                        }
-                        stream.Close();
-                        stream.Dispose();
-                    }
+                    http.BeginGetContext(Callback, http);
                 }
                 catch (Exception ex)
                 {
@@ -77,6 +41,45 @@ namespace client.service.ui.api.service.webServer
                 }
             }, TaskCreationOptions.LongRunning);
         }
+
+        private void Callback(IAsyncResult result)
+        {
+            HttpListener http = result.AsyncState as HttpListener;
+            HttpListenerContext context = http.EndGetContext(result);
+            HttpListenerRequest request = context.Request;
+            HttpListenerResponse response = context.Response;
+
+            try
+            {
+                response.Headers.Set("Server", "snltty");
+                string path = request.Url.AbsolutePath;
+                //默认页面
+                if (path == "/") path = "index.html";
+
+                byte[] bytes = webServerFileReader.Read(path, out DateTime last);
+                if (bytes.Length > 0)
+                {
+                    response.ContentLength64 = bytes.Length;
+                    response.ContentType = GetContentType(path);
+                    response.Headers.Set("Last-Modified", last.ToString());
+                    response.OutputStream.Write(bytes, 0, bytes.Length);
+                    response.OutputStream.Flush();
+                    response.OutputStream.Close();
+                }
+                else
+                {
+                    response.StatusCode = (int)HttpStatusCode.NotFound;
+                }
+            }
+            catch (Exception)
+            {
+                response.StatusCode = (int)HttpStatusCode.BadRequest;
+            }
+
+            response.Close();
+            http.BeginGetContext(Callback, http);
+        }
+
 
 
         private Dictionary<string, string> types = new Dictionary<string, string> {
